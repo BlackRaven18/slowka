c275775e33b7ccb3587e7a4e051c5fce4e58d54b
Arek Wolski <bebe.@gmail.com>
2022-08-22 12:06:15 +0200

    randomly chosen words, checking the correctness of translations

--- src/main/java/com/arek/MainDashboardController.java
+++ src/main/java/com/arek/MainDashboardController.java
@@ -3,21 +3,48 @@ package com.arek;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainDashboardController implements Initializable {
 
+
+
+    // ****************************************************
+    // Translation Tab
+    // ****************************************************
     private WordAndTranslationsManager wordAndTranslationsManager;
 
-    @FXML private Label wordLabel;
+    @FXML private Label wordLabel, messageLabel;
+    @FXML private TextField translationField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         wordAndTranslationsManager = new WordAndTranslationsManager();
+        loadWordLabel();
+    }
 
-        wordLabel.setText(String.valueOf(DatabaseManager.getWordsNumber()));
+    private void loadWordLabel(){
+
+        wordLabel.setText(wordAndTranslationsManager.getRandomWord());
+    }
+
+    @FXML
+    public void checkIfCorrect(){
+
+        //if true
+        if(wordAndTranslationsManager.checkTranslation(wordLabel.getText(), translationField.getText())){
+            messageLabel.setText("OK!!!");
+            translationField.setText("");
+            loadWordLabel();
+        } else{
+            messageLabel.setText("NIE OK!!!");
+        }
+
+        translationField.setText("");
     }
 
 }
--- src/main/java/com/arek/WordAndTranslationsManager.java
+++ src/main/java/com/arek/WordAndTranslationsManager.java
@@ -1,10 +1,11 @@
 package com.arek;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class WordAndTranslationsManager {
     private HashMap<String, ArrayList<String>> wordsAndTranslations;
+    private ArrayList<String> wordsAndTranslationsKeys;
+
 
     public WordAndTranslationsManager(){
         loadWordsAndTranslations();
@@ -12,9 +13,58 @@ public class WordAndTranslationsManager {
 
     private void loadWordsAndTranslations(){
         wordsAndTranslations = DatabaseManager.getWordsAndTranslations();
+        loadWordsAndTranslationsKeys();
+    }
+
+
+    private void loadWordsAndTranslationsKeys(){
+        wordsAndTranslationsKeys = new ArrayList<>(wordsAndTranslations.keySet());
+    }
+
+    public String getRandomWord(){
+        int i;
+
+        Random random = new Random();
+
+        // if there is no more words it should be loaded again
+        if(wordsAndTranslations.size() == 0){
+            loadWordsAndTranslations();
+            loadWordsAndTranslationsKeys();
+        }
+
+        i = random.nextInt(wordsAndTranslations.size());
+        return wordsAndTranslationsKeys.get(i);
+    }
+
+    public boolean checkTranslation(String word, String translation){
+        ArrayList<String> correctTranslations = wordsAndTranslations.get(word);
+
+        for(String correctTranslation : correctTranslations){
+            if(translation.equals(correctTranslation)){
+                //deleting the used word
+                removeUsedWord(word);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void removeUsedWord(String key){
+        wordsAndTranslations.remove(key);
+
+        for(int i = 0; i < wordsAndTranslationsKeys.size(); i++){
+            if(wordsAndTranslationsKeys.get(i).equals(key)){
+                wordsAndTranslationsKeys.remove(i);
+                return;
+            }
+        }
     }
 
     public HashMap<String, ArrayList<String>> getWordsAndTranslations() {
         return wordsAndTranslations;
+    }
+
+    public ArrayList<String> getWordsAndTranslationsKeys() {
+        return wordsAndTranslationsKeys;
     }
 }
